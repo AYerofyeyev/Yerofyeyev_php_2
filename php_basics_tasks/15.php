2e4239aeefc2f7132f79e3737a698d32461f3c3c
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-06-11 01:29:29 +0300

    task list is added

--- php_basics_tasks/15.php
+++ php_basics_tasks/15.php
@@ -1,9 +1,9 @@
 <?php
     echo "<h1>Калькулятор</h1>";
+
     echo "<form action='15.php' target='_self'>";
+
     echo "<input type='text' name='a' placeholder='Введите первое число'>";
-    echo "<br>";
-    echo "<input type='text' name='b' placeholder='Введите второе число'>";
     echo "<br>";
 
     echo "<select name='operator'>";
@@ -30,12 +30,15 @@
     echo "</select>";
     echo "<br>";
 
+    echo "<input type='text' name='b' placeholder='Введите второе число'>";
+    echo "<br>";
+
     echo "<input type='submit' value='Результат: '>";
     echo "</form>";
 
     $a = $_REQUEST['a'];
-    $b = $_REQUEST['b'];
     $operator = $_REQUEST['operator'];
+    $b = $_REQUEST['b'];
 
     if ($operator == '/' | $operator == '%' && $b == 0){
         echo "На ноль делить нельзя!";
@@ -64,5 +67,8 @@
         }
         echo "{$a} {$operator} {$b} = {$result}";
     }
+
+    echo "<br>";
+    echo "<a href='task.php' target='_blank'>К списку заданий</a>";
 
 ?>
