7c66b0d16caa7cca8ab5a189b60e5481617db3e0
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-06-11 14:43:51 +0300

    tasks 21, 22 are added to task 20

--- php_basics_tasks/20.php
+++ php_basics_tasks/20.php
@@ -1,8 +1,10 @@
 <?php
     $a = 20;
-    echo "\$a = {$a}";
+    $b = 0;
+    $c = -20;
+    echo "\$a = {$a}, \$b = {$b},\$c = {$c}";
     echo "<br>";
-    var_dump((bool)$a);
+    var_dump((bool)$a, (bool)$b, (bool)$c);
     echo "<br>";
     echo "Любое число, отличное от 0, приводится к true";
 ?>
